6f40ef4d02e33e5e004df73a6a4979a1fda762d3
unknown <datp1907@gmail.com>
2025-03-25 12:33:01 +0700

    Final fix for purchasedProduct model

--- server/src/services/order.service.js
+++ server/src/services/order.service.js
@@ -7,7 +7,7 @@ const userVoucherModel = require("../models/userVoucher.model");
 const shippingCompany = require("../models/shippingCompany.model");
 const { default: mongoose } = require("mongoose");
 const orderModel = require("../models/order.model"); 
-const purchasedProductModel = require("../models/purchasedProduct.model");
+// const purchasedProductModel = require("../models/purchasedProduct.model");
 
 class OrderService {
   // Hàm tạo đơn hàng mới, nhận payload chứa thông tin đơn hàng
@@ -186,21 +186,21 @@ class OrderService {
       const orderItems = updatedOrder.order_products;
       for (const item of orderItems) {
         // Kiểm tra xem sản phẩm đã được mua trước đó chưa
-        const existingProduct = await purchasedProductModel.findOne({
-          pc_userId: updatedOrder.order_user,
-          pc_productId: item.productId
-        });
+        // const existingProduct = await purchasedProductModel.findOne({
+        //   pc_userId: updatedOrder.order_user,
+        //   pc_productId: item.productId
+        // });
         if (existingProduct) {
           // Nếu sản phẩm đã tồn tại, cập nhật số lượng
           existingProduct.pc_quantity += item.quantity;
           await existingProduct.save();
         } else {
           // Nếu sản phẩm chưa tồn tại, tạo bản ghi mới
-      await purchasedProductModel.create({ pc_userId: updatedOrder.order_user,
-            pc_productId: item.productId,
-            pc_quantity: item.quantity,
-            pc_purchaseDate: new Date()})
-        }
+      // await purchasedProductModel.create({ pc_userId: updatedOrder.order_user,
+      //       pc_productId: item.productId,
+      //       pc_quantity: item.quantity,
+      //       pc_purchaseDate: new Date()})
+      //   }
       }
     }
     return updatedOrder
