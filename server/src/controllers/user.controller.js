f632a4eb495e2ef2cce02b98b2fb9c345337c4c0
unknown <datp1907@gmail.com>
2025-04-05 17:12:35 +0700

    #2 Sprint1 BE- quan ly vai tro, nguoi dung, voucher, đơn hàng

---
+++ server/src/controllers/user.controller.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const UserService = require('../services/user.service');
+
+class UserController {
+    static async addUser(req, res) {
+        const user = await UserService.addUser(req.body);
+        res.status(201).json({
+            success: true,
+            message: 'Thêm người dùng thành công!',
+            data: user,
+        });
+    }
+
+    static async updateUser(req, res) {
+        const updatedUser = await UserService.updateUser(req.params.uid, req.body);
+        res.status(200).json({
+            success: true,
+            message: 'Cập nhật người dùng thành công!',
+            data: updatedUser,
+        });
+    }
+
+    static async deleteUser(req, res) {
+        const deletedUser = await UserService.deleteUser(req.params.uid);
+        res.status(200).json({
+            success: true,
+            message: 'Xóa người dùng thành công!',
+            data: deletedUser,
+        });
+    }
+
+    static async toggleBlockUser(req, res) {
+        const { isBlocked } = req.body;
+        if (typeof isBlocked !== 'boolean') {
+            return res.status(400).json({ success: false, message: 'Trạng thái chặn không hợp lệ!' });
+        }
+        const message = await UserService.toggleBlockUser(req.params.uid, isBlocked);
+        res.status(200).json({
+            success: true,
+            message: message,
+        });
+    }
+
+    static async getAllUsers(req, res) {
+        const users = await UserService.getAllUsers(req.query);
+        res.status(200).json({
+            success: true,
+            message: 'Lấy danh sách người dùng thành công!',
+            data: users,
+        });
+    }
+
+    static async getProfile(req, res) {
+        const profile = await UserService.getProfile(req.user._id);
+        res.status(200).json({
+            success: true,
+            message: 'Lấy thông tin cá nhân thành công!',
+            data: profile,
+        });
+    }
+    static async updateProfile(req, res) {
+        res.status(200).json({
+            success: true,
+            message: 'Cập nhật thông tin cá nhân thành công',
+            data: await UserService.updateProfile(req.user._id, req.body),
+        });
+    }
+}
+
+module.exports = UserController;
