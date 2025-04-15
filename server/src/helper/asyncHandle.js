1ef85a0a3b3b4d5ec84ccfcda3dedd42e545cc45
thaianhtai <thaianhtai167@gmail.com>
2025-04-15 18:54:01 +0700

    search brand ,hiển thị thông báo chặn

--- server/src/helper/asyncHandle.js
+++ server/src/helper/asyncHandle.js
@@ -3,12 +3,11 @@
  * trong Express, giúp tự động bắt lỗi bằng .catch(next)
  */
 const asyncHandle = (fn) => {
+    // fn: route handler hoặc middleware async (req, res, next) => {...}
     return (req, res, next) => {
-        console.log('Checking fn:', fn);  // Log để kiểm tra giá trị của fn
-        if (typeof fn !== 'function') {
-            console.error('fn is not a function');
-            return next(new Error('fn is not a function'));
-        }
+        // Trả về một middleware mới có tham số req, res, next
+        // Gọi fn (async). Nếu fn throw error hoặc reject,
+        // .catch(next) sẽ gọi next(error) => chuyển lỗi sang middleware xử lý lỗi của Express
         Promise.resolve(fn(req, res, next)).catch(next);
     };
 };
