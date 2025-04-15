51fb3e533e401a008f01b7c7deef61cce5a4ba1e
thaianhtai <thaianhtai167@gmail.com>
2025-04-15 10:20:33 +0700

    auth user , cart ,yêu thích sản phẩm

---
+++ server/src/controllers/admin.controller.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const AdminService = require('../services/admin.service');
+
+class adminController {
+    static async addAdmin(req, res) {
+        const admin = await AdminService.addAdmin(req.body);
+        res.status(201).json({
+            success: true,
+            message: 'Thêm người dùng thành công!',
+            data: admin,
+        });
+    }
+
+    static async updateAdmin(req, res) {
+        const updatedAdmin = await AdminService.updateAdmin(req.params.uid, req.body);
+        res.status(200).json({
+            success: true,
+            message: 'Cập nhật người dùng thành công!',
+            data: updatedAdmin,
+        });
+    }
+
+    static async deleteAdmin(req, res) {
+        const deletedAdmin = await AdminService.deleteAdmin(req.params.uid);
+        res.status(200).json({
+            success: true,
+            message: 'Xóa người dùng thành công!',
+            data: deletedAdmin,
+        });
+    }
+
+    static async toggleBlockAdmin(req, res) {
+        const { isBlocked } = req.body;
+        if (typeof isBlocked !== 'boolean') {
+            return res.status(400).json({ success: false, message: 'Trạng thái chặn không hợp lệ!' });
+        }
+        const message = await AdminService.toggleBlockAdmin(req.params.uid, isBlocked);
+        res.status(200).json({
+            success: true,
+            message: message,
+        });
+    }
+
+    static async getAllAdmins(req, res) {
+        const admins = await AdminService.getAllAdmins({ admin_id: req.admin._id, ...req.query });
+        res.status(200).json({
+            success: true,
+            message: 'Lấy danh sách người dùng thành công!',
+            data: admins,
+        });
+    }
+
+    static async getProfile(req, res) {
+        const profile = await AdminService.getProfile(req.admin._id);
+        res.status(200).json({
+            success: true,
+            message: 'Lấy thông tin cá nhân thành công!',
+            data: profile,
+        });
+    }
+    static async updateProfile(req, res) {
+        res.status(200).json({
+            success: true,
+            message: 'Cập nhật thông tin cá nhân thành công',
+            data: await AdminService.updateProfile(req.admin._id, req.body),
+        });
+    }
+}
+
+module.exports = adminController;
---
+++ server/src/routes/cart/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const CartController = require("../../controllers/cart.controller");
+const asyncHandle = require("../../helper/asyncHandle");
+const { userAuthentication } = require("../../middlewares/auth.user.middleware");
+
+const router = express.Router();
+
+// Sử dụng middleware userAuthentication cho tất cả các route trong cart
+router.use(userAuthentication);
+
+// Route thêm sản phẩm vào giỏ hàng
+router.post("/add", asyncHandle(CartController.addToCart));
+
+// Route cập nhật số lượng sản phẩm trong giỏ hàng
+router.put("/update", asyncHandle(CartController.updateCart));
+
+// Route xóa sản phẩm khỏi giỏ hàng
+router.delete("/remote", asyncHandle(CartController.removeFromCart));
+
+// Route xóa toàn bộ giỏ hàng của người dùng
+router.delete("/clear", asyncHandle(CartController.clearCart));
+
+// Route lấy giỏ hàng của người dùng
+router.get("/get", asyncHandle(CartController.getCartByUserId));
+
+
+
+module.exports = router;
