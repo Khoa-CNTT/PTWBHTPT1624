ebc612f091e1221b00ad79a9fbea725d25e96afb
thaianhtai <thaianhtai167@gmail.com>
2025-04-25 18:02:54 +0700

    fix code service san pham

--- server/src/services/purchasedProduct.service.js
+++ server/src/services/purchasedProduct.service.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { BadRequestError, NotFoundError } = require('../core/error.response');
+const { RequestError, NotFoundError } = require('../core/error.response');
 const mongoose = require('mongoose');
 const PurchasedModel = require('../models/Purchased.model');
 
@@ -10,7 +10,16 @@ class PurchasedProductService {
         const limitNum = parseInt(limit, 10); // Mặc định limit = 10
         const pageNum = parseInt(page, 10); // Mặc định page = 0
         const skipNum = pageNum * limitNum;
-        const PurchasedProduct = await PurchasedModel.find({ pc_userId: userId }).skip(skipNum).limit(limitNum).lean();
+        const PurchasedProduct = await PurchasedModel.find({ pc_userId: userId })
+            .populate({
+                path: 'pc_productId',
+                select: 'product_thumb product_price product_code product_slug product_name product_discounted_price product_discount',
+            })
+            .sort({ pc_isReviewed: 1, createdAt: -1 }) // true trước, rồi theo thời gian mới nhất
+            .skip(skipNum)
+            .limit(limitNum)
+            .lean();
+
         const totalProducts = await PurchasedModel.countDocuments({ order_user: userId });
         return {
             totalPage: Math.ceil(totalProducts / limitNum) - 1, // Tổng số trang (0-based)
