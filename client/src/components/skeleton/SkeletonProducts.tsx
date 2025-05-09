396d67733a1042e3b21bc66f5e40dba241e3ed9a
thaianhtai <thaianhtai167@gmail.com>
2025-05-09 11:39:49 +0700

    code vaf update theem Skeletonm

--- client/src/components/skeleton/SkeletonProducts.tsx
+++ client/src/components/skeleton/SkeletonProducts.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const SkeletonProducts: React.FC<{ index: number }> = ({ index }) => {
     return (
-        <div className="grid mobile:grid-cols-2 tablet:grid-cols-4 grid-cols-6  z-0">
+        <div className="grid mobile:grid-cols-2 tablet:grid-cols-2 grid-cols-6  z-0">
             {[...Array(index)].map(() => (
                 <div className="flex flex-col w-full h-full  px-3 pb-6 ">
                     <div className="flex w-[90%] h-[170px] mx-auto">
